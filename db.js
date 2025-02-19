63e93faa2ac49e2359427596dd0503543d3a3acb
*AbhinavSingh93* <*abhinavsingh18813@gmail.com*>
2025-02-20 00:14:00 +0530

    JWT Learning

---
+++ db.js
@@ -0,0 +1,42 @@
+const users=[
+  {
+    email:"abhinavsingh18813@gmail.com",
+    password:"123456789"
+  }
+]
+
+const publicPosts=[
+  {
+    title:"Free Tips on developement",
+    content:"These are some tips"
+  },
+  {
+    title:"Free Tips on developement",
+    content:"These are some tips"
+  },
+  {
+    title:"Free Tips on developement",
+    content:"These are some tips"
+  }
+]
+
+const privatePosts=[
+  {
+    title:"Paid Tips on developement",
+    content:"These are some tips"
+  },
+  {
+    title:"Paid Tips on developement",
+    content:"These are some tips"
+  },
+  {
+    title:"Paid Tips on developement",
+    content:"These are some tips"
+  }
+]
+
+module.exports={
+  users,
+  publicPosts,
+  privatePosts
+}
